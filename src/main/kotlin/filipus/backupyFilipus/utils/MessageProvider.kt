fad626cf868d94d3d7723aaa60a231f5a7b29f06
f1lipuss <fairout60@gmail.com>
2025-06-18 18:06:33 +0200

    Update MessageProvider.kt

--- src/main/kotlin/filipus/backupyFilipus/utils/MessageProvider.kt
+++ src/main/kotlin/filipus/backupyFilipus/utils/MessageProvider.kt
@@ -1,1 +1,18 @@
+package filipus.backupyFilipus.utils
 
+import org.bukkit.configuration.file.FileConfiguration
+import net.md_5.bungee.api.ChatColor
+
+class MessageProvider(private val config: FileConfiguration) {
+    fun get(key: String, placeholders: Map<String, String> = emptyMap()): String {
+        val message = config.getString("messages.$key") ?: return "§cBrak wiadomości dla '$key'"
+        val replacedMessage = placeholders.entries.fold(message) { acc, (placeholder, value) ->
+            acc.replace("%$placeholder%", value.escapeFormatting())
+        }
+        return ChatColor.translateAlternateColorCodes('&', replacedMessage)
+    }
+
+    private fun String.escapeFormatting(): String {
+        return replace("&", "&&").replace("§", "&§")
+    }
+}
