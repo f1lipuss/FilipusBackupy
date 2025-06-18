467659cac542317cb10f70cba16171b3cbb16395
f1lipuss <fairout60@gmail.com>
2025-06-18 18:06:49 +0200

    Update BackupyFilipus.kt

--- src/main/kotlin/filipus/backupyFilipus/BackupyFilipus.kt
+++ src/main/kotlin/filipus/backupyFilipus/BackupyFilipus.kt
@@ -1,55 +1,78 @@
 package filipus.backupyFilipus
 
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.configuration.file.YamlConfiguration
 import filipus.backupyFilipus.commands.BackupCommand
 import filipus.backupyFilipus.commands.BackupOdbierzCommand
+import filipus.backupyFilipus.commands.BackupyReloadCommand
 import filipus.backupyFilipus.listeners.DeathListener
 import filipus.backupyFilipus.listeners.InventoryListener
 import filipus.backupyFilipus.managers.BackupManager
+import filipus.backupyFilipus.utils.MessageProvider
 import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import java.io.File
 
 class BackupyFilipus : JavaPlugin() {
 
+    lateinit var messages: MessageProvider
     lateinit var backupManager: BackupManager
         private set
 
     override fun onEnable() {
-        // Save default config if it doesn't exist
-        saveDefaultConfig()
+        loadConfig()
+
+        val pendingRestoresFile = File(dataFolder, "pending_restores.yml")
+        if (!pendingRestoresFile.exists()) {
+            saveResource("pending_restores.yml", false)
+        }
+
+        saveResource("messages.yml", false)
+        val messagesConfig = YamlConfiguration.loadConfiguration(File(dataFolder, "messages.yml"))
+        messages = MessageProvider(messagesConfig)
 
         backupManager = BackupManager(this)
 
         registerCommands()
         registerListeners()
 
-        logger.info("BackupyFilipus WYLACZONY")
+        logger.info("777-Backupy włączony")
     }
 
     override fun onDisable() {
-        logger.info("BackupyFilipus WLACZONY")
+        logger.info("777-Backupy wyłączony")
+    }
+
+    private fun loadConfig() {
+        saveDefaultConfig()
+        reloadConfig()
     }
 
     private fun registerCommands() {
         val backupCommand = BackupCommand(this)
         val backupOdbierzCommand = BackupOdbierzCommand(this)
+        val backupyReloadCommand = BackupyReloadCommand(this)
 
-        registerCommand("backup", backupCommand)
-        registerCommand("backupodbierz", backupOdbierzCommand)
-    }
-
-    private fun registerCommand(name: String, executor: org.bukkit.command.CommandExecutor) {
-        val fallbackPrefix = description.name.lowercase()
-        val commandMap = server.commandMap
-        val newCommand = object : Command(name) {
-            override fun execute(sender: org.bukkit.command.CommandSender, commandLabel: String, args: Array<out String>): Boolean {
-                return executor.onCommand(sender, this, commandLabel, args)
-            }
-        }
-        commandMap.register(fallbackPrefix, newCommand)
+        getCommand("backup")?.setExecutor(backupCommand)
+        getCommand("backupodbierz")?.setExecutor(backupOdbierzCommand)
+        getCommand("backupreload")?.setExecutor(backupyReloadCommand)
     }
 
     private fun registerListeners() {
         server.pluginManager.registerEvents(DeathListener(this), this)
         server.pluginManager.registerEvents(InventoryListener(this), this)
+    }
+
+    fun reloadAllConfigs() {
+        reloadConfig()
+
+        val messagesConfig = YamlConfiguration.loadConfiguration(File(dataFolder, "messages.yml"))
+        messages = MessageProvider(messagesConfig)
+
+        val pendingRestoresFile = File(dataFolder, "pending_restores.yml")
+        if (pendingRestoresFile.exists()) {
+            val pendingRestoresConfig = YamlConfiguration.loadConfiguration(pendingRestoresFile)
+            backupManager.reloadPendingRestores(pendingRestoresConfig)
+        }
     }
 }
