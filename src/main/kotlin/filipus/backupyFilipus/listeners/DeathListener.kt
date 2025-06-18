c00317ae47fd56faac52fb4bc7bbaccc4ecd41c8
f1lipuss <fairout60@gmail.com>
2025-06-18 18:04:37 +0200

    Update DeathListener.kt

--- src/main/kotlin/filipus/backupyFilipus/listeners/DeathListener.kt
+++ src/main/kotlin/filipus/backupyFilipus/listeners/DeathListener.kt
@@ -1,14 +1,19 @@
 package filipus.backupyFilipus.listeners
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
 import filipus.backupyFilipus.BackupyFilipus
 
 class DeathListener(private val plugin: BackupyFilipus) : Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onPlayerDeath(event: PlayerDeathEvent) {
         val player = event.entity
-        plugin.backupManager.savePlayerInventory(player)
+        try {
+            plugin.backupManager.savePlayerInventory(player)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
